036f275ecc31216cb0d682f1ed6e5350841b8b2c
Macarena Yurjevic <myurjevic@nisum.com>
2017-09-07 19:32:13 -0300

    Basic UserService and UserServiceTest ready

--- src/main/java/com/nisum/blog/service/UserService.java
+++ src/main/java/com/nisum/blog/service/UserService.java
@@ -10,31 +10,111 @@ public class UserService {
 
     private List<User> userList;
 
-    public UserService()
-    {
+    public UserService() {
         userList = new ArrayList<>();
-        userList.add(new User(1, "Macarena", "Yurjevic", "Miki", "Wazuuup", "myurjevic@gmail.com", "http://miFotoFeliz", new ArrayList<Post>(),new ArrayList<Comment>()));
-        userList.add(new User(2, "Juan", "Lopez", "Juanes", "Programar, comer, dormir, repeat", "juan4eva@gmail.com", "http://fotoViaje", new ArrayList<Post>(),new ArrayList<Comment>()));
-        userList.add(new User(3, "Paulina", "Gamboa", "Pali", "Dispersion", "pgamboa@nisum.com", "http://selfieTrekking", new ArrayList<Post>(),new ArrayList<Comment>()));
+
+        User user1 = new User();
+        user1.setId(1);
+        user1.setFirstName("Macarena");
+        user1.setLastName("Yurjevic");
+        user1.setAlias("Miki");
+        user1.setBio("Wazuup");
+        user1.setEmail("myurjevic@gmail.com");
+        user1.setImage("http://miFotoFeliz");
+        user1.setMyPosts( new ArrayList<Post>());
+        user1.setMyComments(new ArrayList<Comment>());
+
+        User user2 = new User();
+        user2.setId(2);
+        user2.setFirstName("Juan");
+        user2.setLastName("Lopez");
+        user2.setAlias("Juanes");
+        user2.setBio("Programar, comer, dormir, repeat");
+        user2.setEmail("juan4eva@gmail.com");
+        user2.setImage("http://fotoViaje");
+        user2.setMyPosts( new ArrayList<Post>());
+        user2.setMyComments(new ArrayList<Comment>());
+
+        User user3 = new User();
+        user3.setId(3);
+        user3.setFirstName("Paulina");
+        user3.setLastName("Gamboa");
+        user3.setAlias("Pali");
+        user3.setBio("Dispersion");
+        user3.setEmail("pgamboa@nisum.com");
+        user3.setImage("http://selfieTrekking");
+        user3.setMyPosts( new ArrayList<Post>());
+        user3.setMyComments(new ArrayList<Comment>());
+
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
     }
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return userList;
     }
 
-    public User findById(int id)
-    {
+    public User findById(int id) {
         User userFound = new User();
 
-        for (int i = 0; i < userList.size(); i++)
-        {
-            if (userList.get(i).getId() == id)
-            {
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).getId() == id) {
                 userFound = userList.get(i);
                 break;
             }
         }
 
         return userFound;
+    }
+
+    public User findByAlias(String alias) {
+        User userFound = new User();
+
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).getAlias() == alias) {
+                userFound = userList.get(i);
+                break;
+            }
+        }
+
+        return userFound;
+    }
+
+    public List<User> findByFirstName(String firstName) {
+        List<User> usersFound = new ArrayList<>();
+
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).getFirstName() == firstName) {
+                usersFound.add(userList.get(i));
+            }
+        }
+
+        return usersFound;
+    }
+
+    public List<User> findByLastName(String lastName) {
+        List<User> usersFound = new ArrayList<>();
+
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).getLastName() == lastName) {
+                usersFound.add(userList.get(i));
+            }
+        }
+
+        return usersFound;
+    }
+
+    public User findByEmail(String email) {
+        User user = new User();
+
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).getEmail() == email) {
+                user = userList.get(i);
+                break;
+            }
+        }
+
+        return user;
     }
 }
--- src/test/java/com/nisum/blog/service/UserServiceTest.java
+++ src/test/java/com/nisum/blog/service/UserServiceTest.java
@@ -3,26 +3,63 @@ package com.nisum.blog.service;
 import static org.hamcrest.core.Is.*;
 import static org.hamcrest.core.IsEqual.*;
 import static org.junit.Assert.*;
+
+import com.nisum.blog.domain.User;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserServiceTest {
 
     //Todos los usuarios
-    //alias
-    //nombre
-    //apellido
-    //email
-
     @Test
     public void itShouldReturnAllUsers() throws Exception {
         UserService userService = new UserService();
         assertThat(userService.findAll().size(), is(equalTo(3)));
     }
 
+    //User by Id
     @Test
     public void itShouldReturnOneUserById() throws Exception {
         UserService userService = new UserService();
         assertThat(userService.findById(3).getId(), is(equalTo(3)));
     }
+
+    //Users by Alias
+    @Test
+    public void itShouldReturnUserByAlias() throws Exception {
+        UserService userService = new UserService();
+        assertThat(userService.findByAlias("Miki").getId(), is(equalTo(1)));
+    }
+
+    //Users by Name
+    @Test
+    public void itShouldReturnUsersByFirstName() throws Exception {
+        UserService userService = new UserService();
+        List<User> usersFound = userService.findByFirstName("Juan");
+        assertThat(usersFound.size(), is(equalTo(1)));
+    }
+
+    //Users by LastName
+    @Test
+    public void itShouldReturnUsersByLastName() throws Exception {
+        UserService userService = new UserService();
+        List<User> usersFound = userService.findByLastName("Yurjevic");
+        assertThat(usersFound.size(), is(equalTo(1)));
+    }
+
+    //User by email
+    @Test
+    public void itShouldReturnUsersByEmail() throws Exception {
+        UserService userService = new UserService();
+        User user = userService.findByEmail("myurjevic@gmail.com");
+        assertThat(user.getId(), is(equalTo(1)));
+    }
+
+
+
+
+
 
 }
