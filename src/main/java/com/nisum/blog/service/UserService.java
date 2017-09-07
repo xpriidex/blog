0e7373d16d73c32555a2f41f6a307ac6832c783a
Macarena Yurjevic <myurjevic@nisum.com>
2017-09-07 18:21:16 -0300

    UserService and UserServiceTest first draft working

--- src/main/java/com/nisum/blog/service/UserService.java
+++ src/main/java/com/nisum/blog/service/UserService.java
@@ -1,9 +1,10 @@
 package com.nisum.blog.service;
 
+import com.nisum.blog.domain.Comment;
+import com.nisum.blog.domain.Post;
 import com.nisum.blog.domain.User;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class UserService {
 
@@ -12,13 +13,16 @@ public class UserService {
     public UserService()
     {
         userList = new ArrayList<>();
+        userList.add(new User(1, "Macarena", "Yurjevic", "Miki", "Wazuuup", "myurjevic@gmail.com", "http://miFotoFeliz", new ArrayList<Post>(),new ArrayList<Comment>()));
+        userList.add(new User(2, "Juan", "Lopez", "Juanes", "Programar, comer, dormir, repeat", "juan4eva@gmail.com", "http://fotoViaje", new ArrayList<Post>(),new ArrayList<Comment>()));
+        userList.add(new User(3, "Paulina", "Gamboa", "Pali", "Dispersion", "pgamboa@nisum.com", "http://selfieTrekking", new ArrayList<Post>(),new ArrayList<Comment>()));
     }
 
     public List<User> findAll(){
         return userList;
     }
 
-    public User findById(String id)
+    public User findById(int id)
     {
         User userFound = new User();
 
--- src/test/java/com/nisum/blog/service/UserServiceTest.java
+++ src/test/java/com/nisum/blog/service/UserServiceTest.java
@@ -7,16 +7,22 @@ import org.junit.Test;
 
 public class UserServiceTest {
 
+    //Todos los usuarios
+    //alias
+    //nombre
+    //apellido
+    //email
+
     @Test
     public void itShouldReturnAllUsers() throws Exception {
         UserService userService = new UserService();
-        assertThat(userService.findAll().size(), is(equalTo(5)));
+        assertThat(userService.findAll().size(), is(equalTo(3)));
     }
 
     @Test
     public void itShouldReturnOneUserById() throws Exception {
         UserService userService = new UserService();
-        assertThat(userService.findById("3").getId(), is(equalTo("3")));
+        assertThat(userService.findById(3).getId(), is(equalTo(3)));
     }
 
 }
