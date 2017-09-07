a04c7920423c7a424adf616f0ab6f37ea44aa354
Paulina Gamboa <pgamboa@nisum.com>
2017-09-07 13:05:47 -0300

    My first commit

---
+++ src/main/java/com/nisum/blog/domain/User.java
@@ -0,0 +1,4 @@
+package com.nisum.blog.domain;
+
+public class User {
+}
---
+++ src/test/java/com/nisum/blog/domain/CommentTest.java
@@ -0,0 +1,11 @@
+package com.nisum.blog.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CommentTest {
+    @Test
+    public void testDescribeContents() throws Exception {
+    }
+}
