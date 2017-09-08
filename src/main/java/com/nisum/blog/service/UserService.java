ef395d0d7db5cebf4f0c3b1a9908d07c714c18e6
Macarena Yurjevic <myurjevic@nisum.com>
2017-09-08 12:54:50 -0300

    UserServiceTest & UserService refactoring

--- src/main/java/com/nisum/blog/service/UserService.java
+++ src/main/java/com/nisum/blog/service/UserService.java
@@ -12,43 +12,6 @@ public class UserService {
 
     public UserService() {
         userList = new ArrayList<>();
-
-        User user1 = new User();
-        user1.setId(1);
-        user1.setFirstName("Macarena");
-        user1.setLastName("Yurjevic");
-        user1.setAlias("Miki");
-        user1.setBio("Wazuup");
-        user1.setEmail("myurjevic@gmail.com");
-        user1.setImage("http://miFotoFeliz");
-        user1.setMyPosts( new ArrayList<Post>());
-        user1.setMyComments(new ArrayList<Comment>());
-
-        User user2 = new User();
-        user2.setId(2);
-        user2.setFirstName("Juan");
-        user2.setLastName("Lopez");
-        user2.setAlias("Juanes");
-        user2.setBio("Programar, comer, dormir, repeat");
-        user2.setEmail("juan4eva@gmail.com");
-        user2.setImage("http://fotoViaje");
-        user2.setMyPosts( new ArrayList<Post>());
-        user2.setMyComments(new ArrayList<Comment>());
-
-        User user3 = new User();
-        user3.setId(3);
-        user3.setFirstName("Paulina");
-        user3.setLastName("Gamboa");
-        user3.setAlias("Pali");
-        user3.setBio("Dispersion");
-        user3.setEmail("pgamboa@nisum.com");
-        user3.setImage("http://selfieTrekking");
-        user3.setMyPosts( new ArrayList<Post>());
-        user3.setMyComments(new ArrayList<Comment>());
-
-        userList.add(user1);
-        userList.add(user2);
-        userList.add(user3);
     }
 
     public List<User> findAll() {
@@ -116,5 +79,9 @@ public class UserService {
         }
 
         return user;
+    }
+
+    public void add(User user) {
+        userList.add(user);
     }
 }
