e2448662fdfafa35236570a8000e1ddbe3817363
Paulina Gamboa <pgamboa@nisum.com>
2017-09-08 12:38:57 -0300

    commentServiceTest Test update

--- src/test/java/com/nisum/blog/service/CommentServiceTest.java
+++ src/test/java/com/nisum/blog/service/CommentServiceTest.java
@@ -3,6 +3,7 @@ package com.nisum.blog.service;
 import com.nisum.blog.domain.Comment;
 import com.nisum.blog.domain.Post;
 import com.nisum.blog.domain.User;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -12,9 +13,17 @@ import static org.hamcrest.core.Is.*;
 import static org.hamcrest.core.IsEqual.*;
 
 public class CommentServiceTest {
+
+
+    private CommentService commentService;
+
+    @Before
+    public void setUp() throws Exception {
+        commentService = new CommentService();
+    }
+
     @Test
     public void itShouldReturnAllCommentByAuthorId() throws Exception {
-        CommentService commentService = new CommentService();
 
         User user1 = new User();
         user1.setId(1);
@@ -28,14 +37,28 @@ public class CommentServiceTest {
 
     @Test
     public void itShouldReturnAllCommentByAuthorAlias() throws Exception {
-        CommentService commentService = new CommentService();
+
+        User user1 = new User();
+        user1.setAlias("Miki");
+
+        Comment comment1 = new Comment();
+        comment1.setAuthor(user1);
+        commentService.add(comment1);
+
         assertThat(commentService.findByAuthorAlias("Miki").size(), is(equalTo(1)));
     }
 
-    /*@Test
+    @Test
     public void itShouldReturnAllCommentByPostId() throws Exception {
-        CommentService commentService = new CommentService();
-        assertThat(commentService.findAll().size(), is(equalTo(5)));
-    }*/
+
+        Post post1 = new Post();
+        post1.setId(1);
+
+        Comment comment1 = new Comment();
+        comment1.setOriginalPost(post1);
+        commentService.add(comment1);
+
+        assertThat(commentService.findByPostId(1).size(), is(equalTo(1)));
+    }
 
 }
