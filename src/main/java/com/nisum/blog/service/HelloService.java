2e9af29fdfce152db8d217b90e57991401157da0
Paulina Gamboa <pgamboa@nisum.com>
2017-09-13 13:43:38 -0300

    Controller added Spring+

---
+++ src/main/java/com/nisum/blog/service/HelloService.java
@@ -0,0 +1,10 @@
+package com.nisum.blog.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    public String greet() {
+        return "Greetting";
+    }
+}
