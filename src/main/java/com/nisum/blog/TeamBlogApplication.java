a0e59e3424294ae3a0c36f5bb615b3285fc05241
Paulina Gamboa <pgamboa@nisum.com>
2017-09-13 13:21:33 -0300

    TeamBlogApplication.java added

---
+++ src/main/java/com/nisum/blog/TeamBlogApplication.java
@@ -0,0 +1,12 @@
+package com.nisum.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamBlogApplication.class, args);
+	}
+}
