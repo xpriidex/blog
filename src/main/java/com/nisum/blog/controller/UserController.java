eb4c4152c38d27879caaaa15d8e0863fe58916e4
Macarena Yurjevic <myurjevic@nisum.com>
2017-09-14 17:33:45 -0300

    UserController delete updated

--- src/main/java/com/nisum/blog/controller/UserController.java
+++ src/main/java/com/nisum/blog/controller/UserController.java
@@ -18,24 +18,22 @@ public class UserController {
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public ResponseEntity<List<User>> findAll() {
-
-        return new ResponseEntity<List<User>>(userService.findAll(),HttpStatus.OK);
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
     }
 
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<User> findById (@PathVariable("id") Integer id) {
-
-        return new ResponseEntity<User>(userService.findById(id),HttpStatus.OK);
+    public ResponseEntity<User> findById(@PathVariable("id") Integer id) {
+        return new ResponseEntity<User>(userService.findById(id), HttpStatus.OK);
     }
 
-    @RequestMapping(path = "/create", method = RequestMethod.POST)
-    public ResponseEntity<Integer> create (
-        @RequestParam("firstName") String firstName,
-        @RequestParam("lastName") String lastName,
-        @RequestParam("alias") String alias,
-        @RequestParam("bio") String bio,
-        @RequestParam("email") String email,
-        @RequestParam("image") String image) {
+    @RequestMapping(path = "/", method = RequestMethod.POST)
+    public ResponseEntity<Integer> create(
+            @RequestParam("firstName") String firstName,
+            @RequestParam("lastName") String lastName,
+            @RequestParam("alias") String alias,
+            @RequestParam("bio") String bio,
+            @RequestParam("email") String email,
+            @RequestParam("image") String image) {
         User newUser = new User();
         newUser.setFirstName(firstName);
         newUser.setLastName(lastName);
@@ -44,7 +42,15 @@ public class UserController {
         newUser.setEmail(email);
         newUser.setImage(image);
         int userId = userService.create(newUser);
-        return new ResponseEntity<Integer>(userId,HttpStatus.OK);
-        //http://localhost:8080/users/create?firstName="Jojo"&lastName="Junes"&alias="Jeans"&bio="myBio"&email="myEmail@gmail.com"&image="laFotoBkn.jpg"
+        return new ResponseEntity<Integer>(userId, HttpStatus.OK);
+        //http://localhost:8080/users/?firstName=Jojo&lastName=Junes&alias=Jeans&bio=myBio&email=myEmail@gmail.com&image=laFotoBkn.jpg
     }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity delete(@PathVariable("id") Integer id) {
+        userService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).body("deleted");
+        //http://localhost:8080/users/1
+    }
+
 }
